e3c794e5357c4073cdb72eff62931c0f5ac37356
PSindhuri <35705435+PSindhuri@users.noreply.github.com>
2018-06-20 00:59:28 -0700

    uploading personal website
    Description of uploading personal website

---
+++ index.js
@@ -0,0 +1,71 @@
+
+
+$(document).ready(function(){
+    $("#bigdata").on("click", function(){
+        window.open('https://github.com/PSindhuri/Big-Data-Project','githubProjects');
+    });
+});
+
+
+
+$(document).ready(function(){
+    $("#shopping").on("click", function(){
+        window.open('https://github.com/PSindhuri/Shopping-website-for-watches','githubProjects');
+    });
+});
+
+
+$(document).ready(function(){
+    $("#mlres").on("click", function(){
+
+        window.open('https://github.com/PSindhuri/Research-Project','githubProjects');
+    });
+});
+
+
+
+
+
+
+
+$(document).ready(function(){
+    $("#ios").on("click", function(){
+        window.open('https://github.com/PSindhuri/iOS-Mobile-App','githubProjects');
+    });
+});
+
+
+$(document).ready(function(){
+    $("#full").on("click", function(){
+        window.open('https://github.com/PSindhuri/','githubProjects');
+    });
+});
+
+
+
+$(document).ready(function(){
+    $("#mass").on("click", function(){
+        window.open('https://github.com/PSindhuri/','githubProjects');
+    });
+});
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+    $("#ios").on("click", function(){
+        window.open('https://github.com/PSindhuri/iOS-Mobile-App','githubProjects');
+    });
+});
+
+$(document).ready(function(){
+	$("#divein").on("click",function(){
+		window.location.href = "#AboutMe"
+	});
+});
